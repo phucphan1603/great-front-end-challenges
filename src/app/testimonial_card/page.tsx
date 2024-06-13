9329db05b9aded327aa518e682f7a005f6573b8b
phuc.phannguyenhoai@gmail.com <phuc.phannguyenhoai@gmail.com>
2024-06-13 16:12:35 +0700

    first challenge: Add Testimonial Card

---
+++ src/app/testimonial_card/page.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import React from "react";
+
+export default function TestimonialCard() {
+  return (
+    <div className="h-screen w-screen overflow-hidden bg-gradient-to-tr from-[#F9FAFB] to-[#D2D6DB] md:p-8 p-4">
+      <div className="mt-[200px] w-[340px] mx-auto p-6 bg-white rounded-lg shadow-md">
+        <div className="flex gap-4">
+          <div className="flex-shrink-0">
+            <Image
+              width={48}
+              height={48}
+              src="https://picsum.photos/300/300"
+              alt="Avatar"
+              className="rounded-full"
+            />
+          </div>
+          <div className="flex flex-col justify-between">
+            <p className="text-neutral-900 font-semibold line-clamp-2">
+              Sarah Dole
+            </p>
+            <p className="text-neutral-600 text-sm">@sarahdole</p>
+          </div>
+        </div>
+        <p className="text-neutral-600 mt-4">
+          I've been searching for high-quality abstract images for my design
+          projects, and I'm thrilled to have found this platform. The variety
+          and depth of creativity are astounding!
+        </p>
+      </div>
+    </div>
+  );
+}
